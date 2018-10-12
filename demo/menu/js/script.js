3fe6c301a59b2e2b7ad6bdc3d26fe0ade4878934
BCGen <bcgenever7@gmail.com>
2018-10-12 14:47:13 +0800

    更新 demo/menu

--- demo/menu/js/script.js
+++ demo/menu/js/script.js
@@ -1,5 +1,18 @@
-var grid = new Muuri(".grid", {
-});
+var ITEM_CONTENT_CLASS = ".item-content";
+var ITEM_CLASS = ".item";
+var GRID_CLASS = ".grid";
 
-var btn = document.getElementById("btn");
-var menu = document.getElementById("menu");
+var grid = new Muuri(".grid", {});
+
+var itemContent = document.querySelectorAll(ITEM_CLASS);
+console.log(grid);
+
+itemContent.forEach(element => {
+  element.onclick = function() {
+    itemContent.forEach(e => {
+      e.classList.add("item_small");
+    });
+    grid.refreshItems();
+    grid.layout();
+  };
+});
