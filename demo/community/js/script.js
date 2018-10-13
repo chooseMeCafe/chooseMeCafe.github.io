6415f64c9f72a41e366adc771f5c029b8093cf71
BCGen <bcgenever7@gmail.com>
2018-10-13 11:25:14 +0800

    修改 menu/community
    修改media max-width至768px
    swiper禁止使用滑鼠切換圖片

--- demo/community/js/script.js
+++ demo/community/js/script.js
@@ -9,39 +9,39 @@ var swiperCenter = new Swiper(SWIPER_CENTER_CLASS, getOption(400));
 var swiperRight = new Swiper(SWIPER_RIGHT_CLASS, getOption(400));
 
 igPhotos.forEach(element => {
-  element.onclick = function() {
-    window.open("https://www.instagram.com/chooseme_cafe/");
-  };
+    element.onclick = function() {
+        window.open("https://www.instagram.com/chooseme_cafe/");
+    };
 });
 
 swiperCenter.autoplay.stop();
 swiperRight.autoplay.stop();
 
 swiperLeft.on("slideChange", function() {
-  swiperCenter.autoplay.start();
+    swiperCenter.autoplay.start();
 });
 
 swiperCenter.on("slideChange", function() {
-  swiperCenter.autoplay.stop();
-  swiperRight.autoplay.start();
+    swiperCenter.autoplay.stop();
+    swiperRight.autoplay.start();
 });
 
 swiperRight.on("slideChange", function() {
-  swiperRight.autoplay.stop();
+    swiperRight.autoplay.stop();
 });
 
 function getOption(delay = 4000) {
-  return {
-    effect: "fade",
-    speed: 1000,
-    grabCursor: true,
-    onlyExternal: true,
-    cubeEffect: {
-      shadow: false
-    },
-    loop: true,
-    autoplay: {
-      delay: delay
-    }
-  };
+    return {
+        effect: "fade",
+        allowTouchMove: false,
+        speed: 1000,
+        grabCursor: true,
+        cubeEffect: {
+            shadow: false
+        },
+        loop: true,
+        autoplay: {
+            delay: delay
+        }
+    };
 }
