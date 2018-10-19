27190e533d25dcbf38ec1f8a840d0d07d9892d00
BCGen <bcgenever7@gmail.com>
2018-10-20 00:53:01 +0800

    demo/navbar 修改nav banner

--- demo/navbar/js/script.js
+++ demo/navbar/js/script.js
@@ -1,27 +1,28 @@
 // section_navbar 滾動效果
 
-var intViewportHeight = window.innerHeight;
-var navFixHeight = intViewportHeight - 109;
+// var intViewportHeight = window.innerHeight;
+// var navFixHeight = intViewportHeight - 109;
 
-window.onscroll = function () { myFunction() };
+// window.onscroll = function () { myFunction() };
 
-function myFunction() {
-    if (document.body.scrollTop > navFixHeight || document.documentElement.scrollTop > navFixHeight) {
-        /*當卷軸滾動超過50px時，執行以下程式*/
-        document.querySelector('.navbar').classList.add('fixed');
-        document.querySelector('#section_banner').classList.add('fixed');
-    } else {
-        document.querySelector('.navbar').classList.remove('fixed');
-        document.querySelector('#section_banner').classList.remove('fixed');
-    }
-}
+// function myFunction() {
+//     if (document.body.scrollTop > navFixHeight || document.documentElement.scrollTop > navFixHeight) {
+//         /*當卷軸滾動超過50px時，執行以下程式*/
+//         document.querySelector('.navbar').classList.add('fixed');
+//         document.querySelector('#section_banner').classList.add('fixed');
+//     } else {
+//         document.querySelector('.navbar').classList.remove('fixed');
+//         document.querySelector('#section_banner').classList.remove('fixed');
+//     }
+// }
 
 //section_banner 
 var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     centeredSlides: true,
+    loop: true,
     autoplay: {    
-        delay: 2500,
+        delay: 4000,
         disableOnInteraction: false,
     },
     pagination: {
