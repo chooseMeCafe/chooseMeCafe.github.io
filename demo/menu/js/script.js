264aaecd87e0647c1bacd89b43b233184a6fffba
wen0124 <wangwen0124@gmail.com>
2018-10-20 16:08:11 +0800

    修改 demo/menu 新增按鈕滾動事件

--- demo/menu/js/script.js
+++ demo/menu/js/script.js
@@ -1,10 +1,22 @@
 "use strict";
-const btn = document.querySelector('.btn');
-const test = document.querySelector('.container');
+// const btn = document.querySelector('.btn');
+// const test = document.querySelector('.container');
 
-btn.addEventListener('click', function () {
-    test.scrollIntoView({
+// btn.addEventListener('click', function () {
+//     test.scrollIntoView({
+//         block: 'start',
+//         behavior: 'smooth'
+//     });
+// });
+
+const btns = document.querySelectorAll(".btn");
+const menus = document.querySelectorAll(".container");
+
+btns.forEach(function (btn, index) {
+    btn.addEventListener('click', function () {
+        menus[index].scrollIntoView({
         block: 'start',
         behavior: 'smooth'
+        })
     });
 });
