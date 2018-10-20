423b75204e394570117c796eff2cdc2bc2d22315
meizi07 <rainy2525777@gmail.com>
2018-10-20 15:53:34 +0800

    btn

--- demo/menu/js/script.js
+++ demo/menu/js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+const btn = document.querySelector('.btn');
+const test = document.querySelector('.container');
+
+btn.addEventListener('click', function () {
+    test.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth'
+    });
+});
