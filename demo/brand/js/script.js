cf0710fca0db9d483d54a73ee29c368cccc0873f
dodorara <ava2299@icloud.com>
2018-10-22 14:24:41 +0800

    V11

--- demo/brand/js/script.js
+++ demo/brand/js/script.js
@@ -3,15 +3,15 @@ AOS.init({
 });
 
 // 字體切換
-const fontChangeBtn = document.getElementById("font_change_btn");
-const body = document.body;
+// const fontChangeBtn = document.getElementById("font_change_btn");
+// const body = document.body;
 
-fontChangeBtn.onclick = function() {
-  if (body.classList.contains("tex_yen")) {
-    body.classList.remove("tex_yen");
-    fontChangeBtn.textContent = "仿宋";
-  } else {
-    body.classList.add("tex_yen");
-    fontChangeBtn.textContent = "圓體";
-  }
-};
+// fontChangeBtn.onclick = function() {
+//   if (body.classList.contains("tex_yen")) {
+//     body.classList.remove("tex_yen");
+//     fontChangeBtn.textContent = "仿宋";
+//   } else {
+//     body.classList.add("tex_yen");
+//     fontChangeBtn.textContent = "圓體";
+//   }
+// };
