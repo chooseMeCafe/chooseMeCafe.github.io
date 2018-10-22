cee85fb40f8a9f7d647b7dddef38548981618ec0
BCGen <bcgenever7@gmail.com>
2018-10-22 09:23:34 +0800

    demo/navbar 整合手機版navbar

---
+++ demo/navbar/js/navbar_mobile.js
@@ -0,0 +1,26 @@
+var t1 = new TimelineMax({ paused: true });
+
+const btn = document.querySelector("#section_navbar_mobile #btn");
+
+t1.to(".section_navbar_mobile .menu", 0.8, {
+  top: "0%",
+});
+
+t1.staggerFrom(
+  ".section_navbar_mobile .menu a",
+  0.8,
+  { x: 200, opacity: 0, ease: Expo.easeOut },
+  0.3
+);
+
+t1.reverse();
+
+btn.onclick = function() {
+  t1.reversed(!t1.reversed());
+};
+
+$(document).on("click", "a", function() {
+  t1.reversed(!t1.reversed());
+});
+
+
