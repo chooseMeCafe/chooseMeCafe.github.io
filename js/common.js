d8b98d783c1fdc7e395f4eb682ce07ecd88a55ce
BCGen <bcgenever7@gmail.com>
2018-10-23 13:05:56 +0800

    專案整合

--- js/common.js
+++ js/common.js
@@ -2,6 +2,7 @@ const sectionCover = CreateSectionSelect("section_cover");
 const sectionNavbarMobile = CreateSectionSelect("section_navbar_mobile");
 const sectionNavbarMenuMobile = CreateSectionSelect("section_navbar_menu_mobile");
 const sectionNavbar = CreateSectionSelect("section_navbar");
+const sectionIntro = CreateSectionSelect("section_intro");
 
 /************
  * 初始化 AOS
--- js/navbar_.js
+++ js/navbar_.js
@@ -1,17 +1,29 @@
 var navOptions = document.querySelectorAll(sectionNavbar(".option"));
 
 navOptions[0].onclick = function() {
+  document.querySelector(sectionIntro(".section_brand")).scrollIntoView({
+    block: "start",
+    behavior: "smooth"
+  });
+};
 
-}
+navOptions[1].onclick = function() {
+  document.querySelector(sectionIntro(".intro_taste")).scrollIntoView({
+    block: "start",
+    behavior: "smooth"
+  });
+};
 
-navOptions[0].onclick = function() {
+navOptions[2].onclick = function() {
+  document.querySelector(sectionIntro(".section_beauty_wall")).scrollIntoView({
+    block: "start",
+    behavior: "smooth"
+  });
+};
 
-}
-
-navOptions[0].onclick = function() {
-
-}
-
-navOptions[0].onclick = function() {
-
-}
+navOptions[3].onclick = function() {
+  document.querySelector(".section_footer").scrollIntoView({
+    block: "start",
+    behavior: "smooth"
+  });
+};
