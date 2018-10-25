d4e00d2780f3edc89e16aac3f98e1a3a59bf93f7
momoyahorziontalline <momo90030@yahoo.com.tw>
2018-10-25 12:11:13 +0800

    update

---
+++ js/menu_mobile.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const MENU_MOBILE_OPTION_CLASS = ".menu_mobile_option";
+const MENU_COLLAPSE = ".menu_collapse";
+const menuMobileOptions = document.querySelectorAll(MENU_MOBILE_OPTION_CLASS);
+const menuMobileCollapses = document.querySelectorAll(MENU_COLLAPSE);
+
+const bgImages = [
+  "./img/menu_mobile/menu_option_appetizer.jpg",
+  "./img/menu_mobile/menu_option_brunch.jpg",
+  "./img/menu_mobile/menu_option_rice.jpg",
+  "./img/menu_mobile/menu_option_pasta.jpg",
+  "./img/menu_mobile/menu_option_dessert.jpg",
+  "./img/menu_mobile/menu_option_drink.jpg"
+];
+
+setBackgroundImages(menuMobileOptions, bgImages);
+
+for (let index = 0; index < menuMobileOptions.length; index++) {
+  menuMobileOptions[index].onclick = function() {
+    $(menuMobileCollapses[index]).collapse("toggle");
+  };
+}
+
+for (let index = 0; index < menuMobileCollapses.length; index++) {
+  let menuMobileCollapse = $(menuMobileCollapses[index]);
+  let menuMobileOption = menuMobileOptions[index];
+
+  menuMobileCollapse.on("show.bs.collapse", function() {
+    this.classList.add("fadeIn");
+  });
+
+  menuMobileCollapse.on("shown.bs.collapse", function() {
+    this.classList.remove("fadeIn");
+    menuMobileOption.scrollIntoView({
+      block: "start",
+      behavior: "smooth"
+    });
+  });
+
+  menuMobileCollapse.on("hide.bs.collapse", function() {
+    this.classList.add("fadeOut");
+  });
+
+  menuMobileCollapse.on("hidden.bs.collapse", function() {
+    this.classList.remove("fadeOut");
+  });
+}
