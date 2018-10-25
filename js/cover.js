15a18c4cf2e760e78a3195cdfcea3237841ffefa
momoyahorziontalline <momo90030@yahoo.com.tw>
2018-10-25 12:08:26 +0800

    update

--- js/cover.js
+++ js/cover.js
@@ -4,46 +4,21 @@
  *  變量設定
  **************/
 const secCoverTl = new TimelineMax();
-const secCoverT2 = new TimelineMax();
 const coverLeft = SEC_COVER + " .cover_left";
 const coverRight = SEC_COVER + " .cover_right";
 const coverLogo = SEC_COVER + " .logo";
 
 /***************
- *  過場動畫
+ *  過場動畫 
  **************/
+secCoverTl.from(coverLeft, 0.4, { height: 0 }, 0)
+  .from(coverRight, 0.4, { height: 0 }, 0)
+  .to(coverLogo, 0.4, { opacity: 1, scale: 2 }, 0.4)
+  .to(SEC_COVER, 0, {height: 0}, 0.5)
+  .to(coverLogo, 0.4, { opacity: 0, scale: 1 }, 1.5)
+  .to(coverLeft, 0.4, { height: 0 }, 2)
+  .to(coverRight, 0.4, { height: 0 }, 2);
 
-// cover();
-unCover();
-// goToMealCover();
-
-// 過場開始
-function cover() {
-  // secCoverTl
-  // .to(coverLeft, 0.4, { height: "100vh" }, 0)
-  // .to(coverRight, 0.4, { height: "100vh" }, 0)
-  // .to(coverLogo, 0.4, { opacity: 1, scale: 2 }, 0.4);
-}
-
-// 過場結束
-function unCover() {
-  secCoverTl
-    .to(SEC_COVER, 0, { height: 0 }, 0)
-    .from(coverLogo, 0.4, { opacity: 1, scale: 2 }, 1)
-    .from(coverLeft, 0.4, { height: "100vh" }, 1.5)
-    .from(coverRight, 0.4, { height: "100vh" }, 1.5);
-}
-
-function goToMealCover() {
-
-  secCoverT2
-    .to(coverLeft, 0.4, { height: "100vh" })
-    .to(coverRight, 0.4, { height: "100vh" })
-    .to(coverLogo, 0.4, { opacity: 1, scale: 2 }, 0.4);
-
-  setTimeout(onT2End, 1000);
-}
-
-function onT2End() {
-  window.location.href = "./menu";
-}
+// tl.to(".logo", 1, {ease:Power4.easeOut, opacity: 1, scale: 2 }, 0.5)
+// .to(".logo", 0.8, {ease:Power2.easeOut, opacity: 0, scale: 0.5 }, 3)
+// .to(".screen", 1, { ease:Power4.easeOut, height: 0, opacity: 0.5 }, 3.6);
